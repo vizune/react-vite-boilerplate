a765db71cecc2cdcb31a232a934c412de310134b
Sasha Richardson <sasha_richardson@hotmail.co.uk>
2025-06-28 13:54:08 +0100

    npm install Tailwind

--- vizune-boilerplate/src/App.jsx
+++ vizune-boilerplate/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div class="flex justify-center">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- vizune-boilerplate/src/main.jsx
+++ vizune-boilerplate/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
+import '/src/styles/tailwind.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
